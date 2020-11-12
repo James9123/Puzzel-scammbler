20b36380655b08a167859003b9d7bb981755975f
James9123 <jamesbryanwagner@gmail.com>
2020-11-12 00:50:33 +0000

    stuff

---
+++ script.js
@@ -0,0 +1,17 @@
+/* Ratio: 0.935252491678348 */
+
+var imgWidth = 25;
+var imgHeight = 20;
+var rows = 4;
+var columns = 5;
+var imgSrc = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fminorfs.files.wordpress.com%2F2014%2F07%2Fpacman-arcade-game.jpg&f=1&nofb=1"
+
+$( document ).ready(function() {
+    
+
+$(function(){
+ $("#pieces > .imgContainer").draggable();
+});
+
+
+});
