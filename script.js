471ccc9d461b12c00ca047ffd53d9541bd1fe029
James9123 <jamesbryanwagner@gmail.com>
2020-11-19 01:14:30 +0000

    randomize

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
 /* Ratio: 0.935252491678348 */
-
 var imgWidth = 25;
 var imgHeight = 20;
 var rows = 4;
 var columns = 5;
 var imgSrc = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fminorfs.files.wordpress.com%2F2014%2F07%2Fpacman-arcade-game.jpg&f=1&nofb=1"
 
-$( document ).ready(function() {
-    
 
-$(function(){
- $("#pieces > .imgContainer").draggable();
-});
+function randomize(puzzle) {
+  for (var i = puzzle.length - 1; i > 0; i--) {
+    var idx = Math.floor(Math.random() * i);
+    var temp = puzzle[idx];
+    puzzle[idx] = puzzle[i];
+    puzzle[i] = temp;
+  }
+}
 
 
-});
+function init(){
+$("h1").html("Solve the puzzle!")
+$("#final").hide()
+
+var puzzle = []
+
+for(var i = 0;i<rows;i++){
+  for(var j = 0;j<columns; j++){
+ puzzle.push("<div class='"+(i*columns+j)+" imgContainer'>"+
+"<img src="+imgSrc+
+" style='margin-left: -"+(j*imgWidth/columns)+"rem;"+
+"margin-top: -"+(i*imgHeight/rows)+"rem;'></img></div>");
+
+$("#drop-zone").append("<div class='"+(i*columns+j)+
+" imgContainer'></div>");   
+  }
+}
+randomize(puzzle)
+
+puzzle.forEach(function(piece){
+  $("#pieces").append(piece)
+})
+
+
+
+ $(".imgContainer").css({
+    "width": imgWidth / columns + "rem",
+    "height": imgHeight / rows + "rem"
+  });
+
+
+$("#pieces>.imgContainer").draggable()
+}
+
+
+$(document).ready(function(){
+$("#final").click(init)
+})
